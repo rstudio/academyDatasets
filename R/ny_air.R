23782ad6ae14364c64bcbbeb38c6f46eab45816a
skaltman <skaltman@stanford.edu>
2021-05-27 15:59:57 -0700

    add ny_air dataset

---
+++ R/ny_air.R
@@ -0,0 +1,14 @@
+#' Daily predicted New York air quality
+#'
+#' @description
+#'
+#' Daily predictions of PM2.5 (inhalable particulate matter) concentrations for
+#' New York state in 2016, by county.
+#'
+#' @source <https://data.cdc.gov/Environmental-Health-Toxicology/Daily-PM2-5-Concentrations-All-County-2001-2016/7vdq-ztk9>
+#'
+#' @format `r rd_describe_format(ny_air, ny_air_dictionary)`
+"ny_air"
+
+#' @describeIn ny_air The `ny_air` data dictionary
+"ny_air_dictionary"
