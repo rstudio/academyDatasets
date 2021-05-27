d8bdd7758f128baeee07f2991cbc27f85fb91633
Garrett Grolemund <grolemund@gmail.com>
2021-05-27 14:49:28 -0500

    Adds ae data set

---
+++ R/ae.R
@@ -0,0 +1,13 @@
+#' SDTM Adverse Events data
+#'
+#' @description `ae` describes adverse event data related to a simulated
+#'   clinical trial. An adverse event is an undesirable medical occurrence that
+#'   happens while a subject is enrolled in a clinical trial. These occurrences
+#'   must be reported and analyzed to see if they are caused by the treatment
+#'   under study.
+#'
+#' @source <https://rhoinc.github.io/data-library/>
+#' @family Rho SDTM datasets
+#'
+#' @format `r rd_describe_format(ae, ae_dictionary)`
+"ae"
