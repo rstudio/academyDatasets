098840edd47c2955c14773a2928d385a5a94544b
Garrett Grolemund <grolemund@gmail.com>
2021-05-28 10:29:06 -0400

    Adds sv data set

---
+++ R/sv.R
@@ -0,0 +1,13 @@
+#' SDTM Subject Visits data
+#'
+#' @description `sv` describes the clinical visits of subjects in
+#'   a simulated clinical trial.
+#'
+#' @source <https://rhoinc.github.io/data-library/>
+#' @family Rho SDTM datasets
+#'
+#' @format `r rd_describe_format(sv, sv_dictionary)`
+"sv"
+
+#' @describeIn sv The `sv` data dictionary
+"sv_dictionary"
