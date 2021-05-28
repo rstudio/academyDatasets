7c2da710d611b11fff24f0d416eebcb2c1307dcc
Garrett Grolemund <grolemund@gmail.com>
2021-05-28 10:25:41 -0400

    Adds cm dataset

---
+++ R/cm.R
@@ -0,0 +1,17 @@
+#' SDTM Concomitant Medication data
+#'
+#' @description `cm` describes concomitant medication data related to a
+#'   simulated clinical trial. Concomitant medications are medications that
+#'   subjects take during the trial, concomitant with the treatment being
+#'   studied. Concomitant medications are not part of the treatment being
+#'   studied but may be confounding variables, or cause interaction effects or
+#'   adverse events.
+#'
+#' @source <https://rhoinc.github.io/data-library/>
+#' @family Rho SDTM datasets
+#'
+#' @format `r rd_describe_format(cm, cm_dictionary)`
+"cm"
+
+#' @describeIn cm The `cm` data dictionary
+"cm_dictionary"
