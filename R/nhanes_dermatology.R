ae0a2d8f28547ee196f930502c96946099ee4575
Alex Rossell Hayes <44556601+rossellhayes@users.noreply.github.com>
2021-08-05 10:45:45 -0700

    Add `nhanes_dermatology` (#26)

---
+++ R/nhanes_dermatology.R
@@ -0,0 +1,28 @@
+#' National Health and Nutrition Examination Survey, Dermatology, 2017-2018
+#'
+#' @description
+#'
+#' From the [NCHS](https://wwwn.cdc.gov/nchs/nhanes/continuousnhanes/overview.aspx?BeginYear=2017):
+#'
+#' The National Center for Health Statistics (NCHS), Division of Health and
+#' Nutrition Examination Surveys (DHANES), part of the Centers for Disease
+#' Control and Prevention (CDC), has conducted a series of health and nutrition
+#' surveys since the early 1960's. The National Health and Nutrition Examination
+#' Surveys (NHANES) were conducted on a periodic basis from 1971 to 1994. In
+#' 1999, NHANES became continuous. Every year, approximately 5,000 individuals
+#' of all ages are interviewed in their homes and complete the health
+#' examination component of the survey. The health examination is conducted in a
+#' mobile examination center (MEC); the MEC provides an ideal setting for the
+#' collection of high quality data in a standardized environment.
+#'
+#' The dermatology questionnaire section provides personal interview data on sun
+#' exposure and sun protective behavior.
+#'
+#' @source <https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx?Component=Questionnaire&Cycle=2017-2018>
+#' @family NHANES datasets
+#'
+#' @format `r rd_describe_format(nhanes_dermatology, nhanes_dermatology_dictionary)`
+"nhanes_dermatology"
+
+#' @describeIn nhanes_dermatology The `nhanes_dermatology` data dictionary
+"nhanes_dermatology_dictionary"
