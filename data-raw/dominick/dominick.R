01f3eebe07690456bdf3f7d0e33e45791a0ce6bb
Alex Rossell Hayes <alexander@rossellhayes.com>
2021-08-25 13:56:39 -0700

    Add `dominick` dataset

--- data-raw/dominick/dominick.R
+++ data-raw/dominick/dominick.R
@@ -1,14 +1,82 @@
 # Setup ----
 pkgload::load_all()
-library(archive)   # install.packages("archive")
-library(dplyr)     # install.packages("dplyr")
-library(incase)    # install.packages("incase")
-library(lubridate) # install.packages("lubridate")
-library(magrittr)  # install.packages("magrittr")
-library(readr)     # install.packages("readr")
-library(purrr)     # install.packages("purrr")
-library(stringr)   # install.packages("stringr")
-library(tsibble)   # install.packages("tsibble")
+library(archive) # install.packages("archive")
+library(dplyr)
+library(fauxnaif) # install.packages("fauxnaif")
+library(haven)
+library(incase) # install.packages("incase")
+library(lubridate)
+library(magrittr)
+library(readr)
+library(purrr)
+library(stringr)
+library(tsibble) # install.packages("tsibble")
+
+# Department sales ----
+
+dominick_url <-
+  "https://www.chicagobooth.edu/boothsitecore/docs/dff/store-demos-customer-count/ccount_stata.zip"
+
+dominick_raw <- archive::archive_read(dominick_url) %>%
+  read_dta()
+
+dominick <- dominick_raw %>%
+  filter(
+    str_detect(date, "\\d{6}"),
+    store %in% c(
+      2, 4:5, 8:9, 12, 14, 18:19, 21, 25, 28, 32:33, 39:40, 44:56, 59:60, 62,
+      64:65, 67:78, 80:81, 83:84, 86, 88:95, 97:98, 100:119, 121:124, 126,
+      128:134, 137, 139
+    )
+  ) %>%
+  select(
+    date, store, custcoun, grocery, gm,
+    everything(), -mvpclub, -week, -promo, -bottle, -bulk, -convfood,
+    -ends_with("coup"), -ends_with("cp"),  -starts_with("ftg"),
+    -matches("deli."), -matches("meat."), -matches("video.")
+  ) %>%
+  mutate(
+    store    = switch_case(store, 69 ~ 137, preserve = TRUE),
+    date     = paste0("19", date) %>% as.Date("%Y%m%d"),
+    custcoun = as.integer(custcoun),
+    across(where(is.numeric), na_if_in, ~ . < 0)
+  )
+
+dominick_dictionary <- describe_dataset(
+  dominick,
+  .title = "Dominick's Finer Foods department sales data",
+  date = "Date",
+  store = "Store number",
+  custcoun = "Number of customers",
+  grocery = "Non-specialty grocery sales in dollars",
+  gm = "General merchandising sales in dollars",
+  dairy = "Dairy sales in dollars",
+  frozen = "Frozen food sales in dollars",
+  meat = "Meat sales in dollars",
+  fish = "Fish sales in dollars",
+  produce = "Produce sales in dollars",
+  saladbar = "Salad bar sales in dollars",
+  floral = "Floral sales in dollars",
+  deli = "Deli sales in dollars",
+  cheese = "Cheese case sales in dollars",
+  bakery = "Bakery sales in dollars",
+  pharmacy = "Pharmacy sales in dollars",
+  jewelry = "Jewelry sales in dollars",
+  cosmetics = "Cosmetics sales in dollars",
+  haba = "Health and beauty aids sales in dollars",
+  camera = "Camera sales in dollars",
+  photofin = "Photo development sales in dollars",
+  video = "Video sales in dollars",
+  beer = "Beer sales in dollars",
+  wine = "Wine sales in dollars",
+  spirits = "Alcoholic spirits sales in dollars"
+)
+
+usethis::use_data(
+  dominick, dominick_dictionary, overwrite = TRUE
+)
+
+# Product sales ----
 
 read_dominick <- function(upc_url, movement_url) {
   upc <- read_csv(upc_url) %>%
