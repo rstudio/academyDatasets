5cf7ecc2dad8dfa16d3c1d8f855baadabce6a92a
Alex Rossell Hayes <alexander@rossellhayes.com>
2021-08-25 10:04:15 -0700

    Improve documentation

--- R/tourism.R
+++ R/tourism.R
@@ -1,6 +1,15 @@
 #' Kaggle Tourism forecasting competition time series
 #'
-#' 366 monthly time series used in the Kaggle Tourism forecasting competition
+#' @description
+#' From [Athanasopoulos, Hyndman, Song, and Wu (2010)](https://robjhyndman.com/papers/forecompijf.pdf)
+#'
+#' The data include 366 monthly series. They were supplied by both tourism
+#' bodies (such as Tourism Australia, the Hong Kong Tourism Board and Tourism
+#' New Zealand) and various academics, who had used them in previous tourism
+#' forecasting studies.
+#'
+#' In order to adhere to all confidentiality agreements with all parties, the
+#' data are presented under coded titles
 #'
 #' @source <https://zenodo.org/record/4656096>
 #'
--- data-raw/tourism/tourism.R
+++ data-raw/tourism/tourism.R
@@ -10,16 +10,16 @@ tourism <- archive::archive_read(url) %>%
   read_tsf(key = "series_name") %>%
   pluck(1) %>%
   select(
-    timestamp = start_timestamp, series = series_name, value = series_value
+    timestamp = start_timestamp, location = series_name, value = series_value
   ) %>%
-  mutate(series = as.factor(series))
+  mutate(location = as.factor(location))
 
 tourism_dictionary <- describe_dataset(
   tourism,
   .title = "366 monthly time series used in the Kaggle Tourism forecasting competition",
   timestamp = "Month",
-  series = "Series number",
-  value = "Value of the series during the specified month"
+  location = "Anonymized location indicator",
+  value = "Observed value of the series during the specified month"
 )
 
 usethis::use_data(tourism, tourism_dictionary, overwrite = TRUE)
