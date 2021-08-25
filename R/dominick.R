0b593b813407426e74b6eb1f8c79fb34c7a50695
Alex Rossell Hayes <alexander@rossellhayes.com>
2021-08-24 17:25:44 -0700

    Add `dominick_oatmeal` and `dominick_soap`

---
+++ R/dominick.R
@@ -0,0 +1,47 @@
+#' Dominick's Finer Foods oatmeal sales data
+#'
+#' @description
+#'
+#' From the [Chicago Booth Kilts Center for Marketing](https://www.chicagobooth.edu/research/kilts/datasets/dominicks):
+#'
+#' From 1989 to 1994, Chicago Booth and Dominick’s Finer Foods entered into a
+#' partnership for store-level research into shelf management and pricing.
+#' Randomized experiments were conducted in more than 25 different categories
+#' throughout all stores in this 100-store chain. As a by-product of this
+#' research cooperation, approximately nine years of store-level data on the
+#' sales of more than 3,500 UPCs is available in this dataset. This data is
+#' unique for the breadth of its coverage and for the information available on
+#' retail margins.
+#'
+#' @source <https://www.chicagobooth.edu/research/kilts/datasets/dominicks>
+#' @family Dominick's datasets
+#'
+#' @format `r rd_describe_format(dominick_oatmeal, dominick_oatmeal_dictionary)`
+"dominick_oatmeal"
+
+#' @describeIn dominick_oatmeal The `dominick_oatmeal` data dictionary
+"dominick_oatmeal_dictionary"
+
+#' Dominick's Finer Foods bath soap sales data
+#'
+#' @description
+#'
+#' From the [Chicago Booth Kilts Center for Marketing](https://www.chicagobooth.edu/research/kilts/datasets/dominicks):
+#'
+#' From 1989 to 1994, Chicago Booth and Dominick’s Finer Foods entered into a
+#' partnership for store-level research into shelf management and pricing.
+#' Randomized experiments were conducted in more than 25 different categories
+#' throughout all stores in this 100-store chain. As a by-product of this
+#' research cooperation, approximately nine years of store-level data on the
+#' sales of more than 3,500 UPCs is available in this dataset. This data is
+#' unique for the breadth of its coverage and for the information available on
+#' retail margins.
+#'
+#' @source <https://www.chicagobooth.edu/research/kilts/datasets/dominicks>
+#' @family Dominick's datasets
+#'
+#' @format `r rd_describe_format(dominick_soap, dominick_soap_dictionary)`
+"dominick_soap"
+
+#' @describeIn dominick_oatmeal The `dominick_soap` data dictionary
+"dominick_soap_dictionary"
---
+++ data-raw/dominick/dominick.R
@@ -0,0 +1,87 @@
+# Setup ----
+pkgload::load_all()
+library(archive)   # install.packages("archive")
+library(dplyr)     # install.packages("dplyr")
+library(incase)    # install.packages("incase")
+library(lubridate) # install.packages("lubridate")
+library(magrittr)  # install.packages("magrittr")
+library(readr)     # install.packages("readr")
+library(purrr)     # install.packages("purrr")
+library(stringr)   # install.packages("stringr")
+library(tsibble)   # install.packages("tsibble")
+
+read_dominick <- function(upc_url, movement_url) {
+  upc <- read_csv(upc_url) %>%
+    rename_with(tolower) %>%
+    select(upc, product = descrip, size) %>%
+    mutate(product = gsub("[^A-Z ]", "", product))
+
+  movement <- archive::archive_read(movement_url) %>%
+    read_csv() %>%
+    rename_with(tolower) %>%
+    filter(as.logical(ok), price != 0) %>%
+    mutate(
+      price = round(price / qty, 2),
+      profit = profit / 100,
+      move = as.integer(move),
+      store = as.integer(store),
+      week = ymd("1989-07-09") + weeks(week)
+    ) %>%
+    left_join(upc, by = "upc") %>%
+    select(week, store, product, size, move, price, profit) %>%
+    group_by(week, store, product, size, price) %>%
+    summarize(
+      profit = round(weighted.mean(profit, move), 2),
+      move   = sum(move)
+    ) %>%
+    ungroup()
+
+  as_tsibble(movement, key = c(store, product, size, price), index = week) %>%
+    arrange(store, product, size, week, price)
+}
+
+# Datasets ----
+
+## Oatmeal ----
+dominick_oatmeal <- read_dominick(
+  "https://www.chicagobooth.edu/-/media/enterprise/centers/kilts/datasets/dominicks-dataset/upc_csv-files/upcoat.csv",
+  "https://www.chicagobooth.edu/-/media/enterprise/centers/kilts/datasets/dominicks-dataset/movement_csv-files/woat.zip"
+)
+
+dominick_oatmeal_dictionary <- describe_dataset(
+  dominick_oatmeal,
+  .title = "Dominick's Finer Foods oatmeal sales data",
+  week = "Week",
+  store = "Store number",
+  product = "Abbreviated product name",
+  size = "Product size",
+  price = "Price per unit in dollars",
+  profit = "Profit per unit in dollars",
+  move = "Number of units sold during the week"
+)
+
+usethis::use_data(
+  dominick_oatmeal, dominick_oatmeal_dictionary, overwrite = TRUE
+)
+
+## Bath soap ----
+dominick_soap <- read_dominick(
+  "https://www.chicagobooth.edu/-/media/enterprise/centers/kilts/datasets/dominicks-dataset/upc_csv-files/upcbat.csv",
+  "https://www.chicagobooth.edu/-/media/enterprise/centers/kilts/datasets/dominicks-dataset/movement_csv-files/wbat.zip"
+)
+
+dominick_soap_dictionary <- describe_dataset(
+  dominick_soap,
+  .title = "Dominick's Finer Foods bath soap sales data",
+  week = "Week",
+  store = "Store number",
+  product = "Abbreviated product name",
+  size = "Product size",
+  price = "Price per unit in dollars",
+  profit = "Profit per unit in dollars",
+  move = "Number of units sold during the week"
+)
+
+usethis::use_data(
+  dominick_soap, dominick_soap_dictionary, overwrite = TRUE
+)
