6a7b3331564acc5156fbb915b114afdb99eb5059
skaltman <skaltman@stanford.edu>
2021-08-25 15:25:46 -0500

    update script

--- data-raw/fred_md/fred_md.R
+++ data-raw/fred_md/fred_md.R
@@ -4,19 +4,12 @@ library(tidyverse)
 # load {academyDatasets} for access to helper functions
 pkgload::load_all()
 
-url_zip <- "https://zenodo.org/record/4654833/files/fred_md_dataset.zip?download=1"
-file_script <- "data-raw/fred_md/convert_tsf_to_tsibble.R"
+url_fred_md <- "https://zenodo.org/record/4654833/files/fred_md_dataset.zip?download=1"
 #===============================================================================
 
-temp <- "data-raw/fred_md/temp.zip"
-download.file(url_zip, destfile = temp)
-unzip(zipfile = "data-raw/fred_md/temp.zip", exdir = fs::path_dir(temp))
-file.remove(temp)
-
-source(file_script)
-
 fred_md <-
-  convert_tsf_to_tsibble("data-raw/fred_md/fred_md_dataset.tsf") %>%
+  archive::archive_read(url_fred_md) %>%
+  read_tsf() %>%
   pluck(1) %>%
   as_tibble() %>%
   select(
