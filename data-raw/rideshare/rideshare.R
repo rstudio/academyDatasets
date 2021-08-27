313af5f565e301d5f79c87d8e10218640d518b24
David Failing <david.failing@gmail.com>
2021-08-27 11:50:09 -0700

    Initial commit (incomplete)

---
+++ data-raw/rideshare/rideshare.R
@@ -0,0 +1,39 @@
+# Retrieve rideshare dataset
+library(dplyr)   # install.packages("dplyr")
+library(purrr)   # install.packages("purrr")
+library(tibble)  # install.packages("tibble")
+library(tidyr)   # install.packages("tidyr")
+library(tsibble) # install.packages("tsibble")
+
+
+# load {academyDatasets} for access to helper functions
+pkgload::load_all()
+
+url_zip <- "https://zenodo.org/record/5122114/files/rideshare_dataset_with_missing_values.zip"
+local_file <- here::here("data-raw", "rideshare", basename(url_zip))
+#===============================================================================
+
+download.file(url_zip, destfile = local_file)
+unzip(zipfile = local_file, exdir = dirname(local_file))
+file.remove(local_file)
+
+rideshare <-
+  read_tsf(paste0(dirname(local_file), "/", "rideshare_dataset_with_missing_values.tsf"),
+           key = "series_name") %>%
+  pluck(1) %>%
+  as_tibble() #%>%
+  select(timestamp = start_timestamp, demand = series_value)
+# SAVE THIS ONE TO AN RDS FILE?
+
+# rideshare_dictionary <- describe_dataset(
+#   rideshare,
+#   .title = "Victoria, Australia Electricity Demand Data",
+#   timestamp = "Datetime of observation",
+#   demand = "Electricity demand (GW)" # https://otexts.com/fpp2/complexseasonality.html
+# )
+#
+# usethis::use_data(
+#   rideshare,
+#   rideshare_dictionary,
+#   overwrite = TRUE
+# )
