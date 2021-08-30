7075848f582591780dc59c79421b7a5458fde23f
David Failing <david.failing@gmail.com>
2021-08-30 14:32:56 -0700

    Finalize documentation.

---
+++ R/rideshare.R
@@ -0,0 +1,21 @@
+#' Hourly Summaries of Rideshare Service Data
+#'
+#' @description
+#' This dataset contains various hourly time series representations of
+#' attributes related to Uber and Lyft rideshare services for various locations
+#' in New York between 2018-11-26 and 2018-12-18.
+#' For a given starting location, provider and service,
+#'
+#' @source <https://zenodo.org/record/5122114#.YS0--9NKiM9>
+#'
+#' @format `r rd_describe_format(rideshare, rideshare_dictionary)`
+#'
+#' @examples
+#' # Convert to `tsibble`
+#' library(tsibble)
+#' rideshare %>%
+#'   as_tsibble(key = c("source_location", "provider_name", "provider_service"), index = timestamp)
+"rideshare"
+
+#' @describeIn rideshare The `rideshare` data dictionary
+"rideshare_dictionary"
--- data-raw/rideshare/rideshare.R
+++ data-raw/rideshare/rideshare.R
@@ -26,15 +26,32 @@ rideshare <-
   rename(timestamp = start_timestamp) %>%
   pivot_wider(names_from = type, values_from = series_value)
 
-# local_rds <- here::here("data-raw", "rideshare", "rideshare.rds")
-# saveRDS(rideshare, file = local_rds)
+local_rds <- here::here("data-raw", "rideshare", "rideshare.rds")
+saveRDS(rideshare, file = local_rds)
 #  readRDS(file = local_rds)
 
 rideshare_dictionary <- describe_dataset(
   rideshare,
-  .title = "Hourly Rideshare Service Data",
-  timestamp = "Datetime of observation",
-  demand = "Electricity demand (GW)" # https://otexts.com/fpp2/complexseasonality.html
+  .title = "Hourly Summaries of Rideshare Service Data",
+  source_location = "Starting point of the ride",
+  provider_name = "Rideshare service provider",
+  provider_service = "Provider-specific ride type identifier",
+  timestamp = "Hour",
+  price_min = "Minimum price estimate for rides in USD",
+  price_mean = "Mean price estimate for rides in USD",
+  price_max = "Maxmimum price estimate for rides in USD",
+  distance_min = "Minimum Distance between source and destination",
+  distance_mean = "Mean Distance between source and destination",
+  distance_max = "Maximum Distance between source and destination",
+  surge_min = "Minimum multiplier by which price was increased, default 1",
+  surge_mean = "Mean multiplier by which price was increased, default 1",
+  surge_max = "Maximum multiplier by which price was increased, default 1",
+  api_calls = "Number of API calls in the hour",
+  temp = "Temperature (F)",
+  rain = "Rain in the last hour (inches)",
+  humidity = "Humidity (%)",
+  clouds = "Cloud cover (0-1)",
+  wind = "Wind speed (mph)"
 )
 
 usethis::use_data(
